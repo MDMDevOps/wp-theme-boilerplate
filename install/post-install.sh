0d625e8542c33e561b100f5f5641a8a8f6a4df2b
Bob Moore <bobjutsu@gmail.com>
2023-12-07 07:17:04 -0600

    Update post-install.sh

--- install/post-install.sh
+++ install/post-install.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-if ! [ -d ../src ]; then
-  cp -R ../vendor/mwf/wp-scripts-config/src ../
-fi
+
 composer update
 composer dump-autoload
+
+if ! [ -d ../src ]; then
+  cp -R ./vendor/mwf/wp-scripts-config/src ./
+  cd ./src && npm install
+fi
+
+rm -fr ./install
